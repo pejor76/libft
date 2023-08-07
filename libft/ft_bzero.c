31c8380533a1d008647775091f2c860a604cbaf4
pejor76 <pejorcr@gmail.com>
2023-08-07 10:42:05 +0200

    1

---
+++ libft/ft_bzero.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pecampos <pecampos@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/18 12:03:38 by pecampos          #+#    #+#             */
+/*   Updated: 2022/09/20 11:51:15 by pecampos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_bzero(char *c, unsigned long n)
+{
+	unsigned long		cont;
+
+	cont = 0;
+	while (cont != n)
+	{
+		c[cont] = 0;
+		cont++;
+	}
+}
+/* 
+int	main(void)
+{
+	char	str[] = "patata";
+
+    printf("%s\n",str);
+	ft_bzero(str,1);
+    printf("%c",str[0]);
+} */
